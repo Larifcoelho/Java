3010f33814455f363a48d162047e920b45c6dba4
laris <larissafcoelho9@gmail.com>
2025-08-12 17:31:23 -0300

    updates

--- src/src/br/com/larissa/ValidaçãoSenhaWhile.java
+++ src/src/br/com/larissa/ValidaçãoSenhaWhile.java
@@ -3,23 +3,36 @@ package src.br.com.larissa;
 import java.util.Scanner;
 
 public class ValidaçãoSenhaWhile {
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         String senhaCorreta = "1234";
         String senhaDigitada = "";
 
+        int maxTentativas = 3;
+        int tentativas = 0;
+        ;
+
         // enquanto a senha digitada for diferente da correta
-        while (!senhaDigitada.equals(senhaCorreta)) {
-            System.out.print("Digite a senha: ");
+        while (!senhaDigitada.equals(senhaCorreta) && tentativas < maxTentativas) {
+            System.out.print("Você tem " + (maxTentativas - tentativas) + " tentativas. Digite sua senha: ");
             senhaDigitada = scanner.nextLine();
 
             if (!senhaDigitada.equals(senhaCorreta)) {
                 System.out.println("Senha incorreta. Tente novamente.");
+
+            }
+            if (senhaDigitada.equals(senhaCorreta)) {
+                System.out.println("Acesso permitido!");
+            } else {
+                System.out.println("Número máximo de tentativas excedido. Acesso negado!");
+            }
+
+
+            scanner.close();
+
         }
     }
-
-        System.out.println("Acesso permitido!");
-        scanner.close();
-    }
 }
+
+
